1ee034cc686d6c56a1081d76c0182f0221e1e19b
Charismatic <omukuyiadickson@gmail.com>
2026-01-07 23:50:41 +0300

    Configured nvim-tree to dynamically set the togglebar width

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,6 +7,18 @@ return {
                 dotfiles = false,     -- keep dotfiles visible, change to true to hide
                 custom = { ".git", ".venv", ".env", "node_modules", "dist" },  -- <-- ignore .git folder
             },
+            view = {
+                width = function()
+                    local total_columns = vim.go.columns
+                    if total_columns <= 100 then
+                        return 20
+                    elseif total_columns <= 200 then
+                        return 25
+                    else
+                        return 30
+                    end
+                end, 
+            },
             renderer = {
                 icons = {
                     show = {
